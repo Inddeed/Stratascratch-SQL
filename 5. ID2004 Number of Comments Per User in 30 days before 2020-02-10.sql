fc1357a7bda1f13ec36522b88ede787830ed8690
Raihan Putra <91410415+Inddeed@users.noreply.github.com>
2025-02-25 06:58:37 +0700

    Update 5. ID2004 Number of Comments Per User in 30 days before 2020-02-10.sql

--- 5. ID2004 Number of Comments Per User in 30 days before 2020-02-10.sql
+++ 5. ID2004 Number of Comments Per User in 30 days before 2020-02-10.sql
@@ -1,4 +1,6 @@
--- Return the total number of comments received for each user in the 30 or less days before 2020-02-10. Don't output users who haven't received any comment in the defined time period.
+-- Return the total number of comments received for each user 
+-- in the 30 or less days before 2020-02-10. 
+-- Don't output users who haven't received any comment in the defined time period.
 
 
 SELECT
