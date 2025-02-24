e4e066aaf95381cb3128b9f51b83674073f05861
Raihan Putra <91410415+Inddeed@users.noreply.github.com>
2025-02-25 06:11:56 +0700

    Update Variables vs Fixed Rate.sql

--- Variables vs Fixed Rate.sql
+++ Variables vs Fixed Rate.sql
@@ -1,4 +1,7 @@
--- Write a SQL query to show each loan and its rate type. For each loan, create two new columns: fixed and variable. If the loan has a fixed rate, put a 1 in the fixed column and a 0 in the variable column. If the loan has a variable rate, do the opposite.
+-- Write a SQL query to show each loan and its rate type. For each loan, 
+-- create two new columns: fixed and variable. If the loan has a fixed rate, 
+-- put a 1 in the fixed column and a 0 in the variable column. 
+-- If the loan has a variable rate, do the opposite.
 
 
 select
