8346fe4c2345510cc42cb85cdddb68a7f434158a
Raihan Putra <91410415+Inddeed@users.noreply.github.com>
2025-02-25 06:14:47 +0700

    Add files via upload

---
+++ 1. ID 2000 Variables vs Fixed Rate.sql
@@ -0,0 +1,9 @@
+-- Write a SQL query to show each loan and its rate type. For each loan, create two new columns: fixed and variable. If the loan has a fixed rate, put a 1 in the fixed column and a 0 in the variable column. If the loan has a variable rate, do the opposite.
+
+
+select
+    loan_id,
+    CASE WHEN rate_type = 'fixed' THEN 1 ELSE 0 END fixed,
+    CASE WHEN rate_type = 'variable' THEN 1 ELSE 0 END variable
+from submissions;
+
---
+++ 2. ID 2001 Share of Loan Balance.sql
@@ -0,0 +1,15 @@
+-- Write a query that returns the rate_type, loan_id, loan balance , and a column that shows with what percentage the loan's balance contributes to the total balance among the loans of the same rate type.
+
+WITH total_balance AS (
+    SELECT rate_type, SUM(balance) total
+    FROM submissions
+    GROUP BY rate_type
+)
+SELECT 
+    s.loan_id,
+    s.rate_type,
+    s.balance,
+    s.balance * 100.0 / t.total balance_share
+FROM submissions s
+LEFT JOIN total_balance t
+ON s.rate_type = t.rate_type
